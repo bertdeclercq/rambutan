2bf9c49c360502378c7859e8c44751c4d7ef65a6
Bert De Clercq <xunobulax@gmail.com>
2019-10-13 20:29:27 +0200

    Update lifecycle and recyclerview version; revert constraintlayout version

---
+++ app/src/test/java/com/xunobulax/rambutan/model/GroupTest.kt
@@ -0,0 +1,12 @@
+package com.xunobulax.rambutan.model
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GroupTest {
+
+    @Test
+    fun getName() {
+    }
+}
---
+++ app/src/test/java/com/xunobulax/rambutan/model/MemberTest.kt
@@ -0,0 +1,20 @@
+package com.xunobulax.rambutan.model
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MemberTest {
+
+    @Test
+    fun getFirstName() {
+    }
+
+    @Test
+    fun getLastName() {
+    }
+
+    @Test
+    fun getEmail() {
+    }
+}
