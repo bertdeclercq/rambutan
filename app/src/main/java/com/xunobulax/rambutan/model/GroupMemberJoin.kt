0c8f9a621f608e401c176097e8cb92c0a1ff3e58
Bert De Clercq <xunobulax@gmail.com>
2019-10-13 11:01:40 +0200

    Format code and add column names for the ids

--- app/src/main/java/com/xunobulax/rambutan/model/GroupMemberJoin.kt
+++ app/src/main/java/com/xunobulax/rambutan/model/GroupMemberJoin.kt
@@ -1,18 +1,29 @@
 package com.xunobulax.rambutan.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 
 
-@Entity(tableName = "group_member_join",
-    primaryKeys = ["groupId", "memberId"],
-    foreignKeys = [ForeignKey(entity = Group::class,
-        parentColumns = arrayOf("id"),
-        childColumns = arrayOf("groupId")), ForeignKey(entity = Member::class,
-        parentColumns = arrayOf("id"),
-        childColumns = arrayOf("memberId"))]
+@Entity(
+    tableName = "group_member_join",
+    primaryKeys = ["group_id", "member_id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = Group::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("group_id")
+        ),
+        ForeignKey(
+            entity = Member::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("member_id")
+        )]
 )
 data class GroupMemberJoin(
+    @ColumnInfo(name = "group_id")
     val groupId: Int,
+
+    @ColumnInfo(name = "member_id")
     val memberId: Int
 )
