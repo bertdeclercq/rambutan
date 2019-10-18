cd2cb1d7bd5b781f29c71a037d7f5f22953ca652
Bert De Clercq <xunobulax@gmail.com>
2019-10-18 14:54:59 +0200

    Member now contains a groupId

--- app/src/main/java/com/xunobulax/rambutan/model/Member.kt
+++ app/src/main/java/com/xunobulax/rambutan/model/Member.kt
@@ -2,19 +2,32 @@ package com.xunobulax.rambutan.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "members")
+@Entity(
+    tableName = "members",
+    foreignKeys = [
+        ForeignKey(
+            entity = Group::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("group_id"),
+            onDelete = ForeignKey.CASCADE
+        )]
+)
 data class Member(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    val id: Int,
 
     @ColumnInfo(name = "first_name")
-    val firstName: String = "",
+    var firstName: String = "",
 
     @ColumnInfo(name = "last_name")
-    val lastName: String = "",
+    var lastName: String = "",
 
-    val email: String = ""
+    var email: String = "",
+
+    @ColumnInfo(name = "group_id")
+    var groupId: Int
 )
