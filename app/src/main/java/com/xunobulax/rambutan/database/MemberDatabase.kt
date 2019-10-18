30a5b2b567e2eff7a1c0d20142a2a77f36323d69
Bert De Clercq <xunobulax@gmail.com>
2019-10-18 14:55:28 +0200

    Database class

--- app/src/main/java/com/xunobulax/rambutan/database/MemberDatabase.kt
+++ app/src/main/java/com/xunobulax/rambutan/database/MemberDatabase.kt
@@ -1,5 +1,49 @@
 package com.xunobulax.rambutan.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.xunobulax.rambutan.model.Group
+import com.xunobulax.rambutan.model.Member
 
-class MemberDatabase {
+
+const val NAME = "member_database"
+
+
+@Database(
+    entities = [
+        Group::class,
+        Member::class],
+    version = 1
+)
+abstract class MemberDatabase : RoomDatabase() {
+
+    abstract val groupDao: GroupDao
+    abstract val memberDao: MemberDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: MemberDatabase? = null
+
+        fun getInstance(context: Context): MemberDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MemberDatabase::class.java,
+                        NAME
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+                INSTANCE = instance
+
+                return instance
+            }
+        }
+    }
 }
