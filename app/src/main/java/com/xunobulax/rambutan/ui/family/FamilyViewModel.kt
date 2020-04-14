534096addedd2dc20df29e409802935bc64752db
Bert De Clercq <xunobulax@gmail.com>
2020-04-14 15:53:13 +0200

    Get people from dao

--- app/src/main/java/com/xunobulax/rambutan/ui/family/FamilyViewModel.kt
+++ app/src/main/java/com/xunobulax/rambutan/ui/family/FamilyViewModel.kt
@@ -1,7 +1,11 @@
 package com.xunobulax.rambutan.ui.family
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.xunobulax.rambutan.data.Person
+import com.xunobulax.rambutan.data.PersonDao
 
-class FamilyViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class FamilyViewModel(personDao: PersonDao) : ViewModel() {
+
+    val people: LiveData<List<Person>> = personDao.getPeople()
 }
