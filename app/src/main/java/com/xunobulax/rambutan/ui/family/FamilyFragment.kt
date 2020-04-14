a74dd0a3214070e38655fa5948059a4840f35598
Bert De Clercq <xunobulax@gmail.com>
2020-04-14 15:56:03 +0200

    Add PersonAdapter to display list

--- app/src/main/java/com/xunobulax/rambutan/ui/family/FamilyFragment.kt
+++ app/src/main/java/com/xunobulax/rambutan/ui/family/FamilyFragment.kt
@@ -6,21 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.xunobulax.rambutan.R
+import androidx.lifecycle.Observer
+import com.xunobulax.rambutan.adapters.PersonAdapter
+import com.xunobulax.rambutan.data.Person
+import com.xunobulax.rambutan.databinding.FragmentFamilyBinding
+import java.time.LocalDate
 
 
 class FamilyFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = FamilyFragment()
-    }
+    // This is a test list
+    private val people = arrayListOf(
+        Person(1, "John", "Doe", "johndoe@email.com", LocalDate.of(2020, 1, 25), 2),
+        Person(2, "Jane", "Doe", "janedoe@email.com", LocalDate.of(2000, 1, 25), 1)
+    )
 
+    //TODO Make a FamilyViewModelFactory
     private val viewModel: FamilyViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.family_fragment, container, false)
+        val binding = FragmentFamilyBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        val adapter = PersonAdapter()
+        binding.personList.adapter = adapter
+        adapter.submitList(people)
+//        subsribeUi(adapter)
+
+        return binding.root
+    }
+
+    private fun subsribeUi(adapter: PersonAdapter) {
+        viewModel.people.observe(
+            viewLifecycleOwner,
+            Observer { people -> adapter.submitList(people) })
     }
 
 }
