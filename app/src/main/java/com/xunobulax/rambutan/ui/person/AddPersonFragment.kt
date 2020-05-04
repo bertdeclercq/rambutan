f9334794cb8ad58d4d3d21aa37a0fabfd75b9e8c
Bert De Clercq <xunobulax@gmail.com>
2020-05-04 10:41:35 +0200

    Use databinding

--- app/src/main/java/com/xunobulax/rambutan/ui/person/AddPersonFragment.kt
+++ app/src/main/java/com/xunobulax/rambutan/ui/person/AddPersonFragment.kt
@@ -1,33 +1,65 @@
 package com.xunobulax.rambutan.ui.person
 
-import androidx.lifecycle.ViewModelProviders
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.xunobulax.rambutan.R
+import com.xunobulax.rambutan.databinding.FragmentAddPersonBinding
+import kotlinx.android.synthetic.main.fragment_add_person.*
+import java.time.LocalDate
 
 
 class AddPersonFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AddPersonFragment()
-    }
-
-    private lateinit var viewModel: AddPersonViewModel
+    private val viewModel: AddPersonViewModel by viewModels()
+    private lateinit var binding: FragmentAddPersonBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_person, container, false)
+        binding = FragmentAddPersonBinding.inflate(inflater, container, false)
+        context ?: binding.root
+
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AddPersonViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.navigateToBirthdayPickerDialog.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                DatePickerDialog(
+                    requireContext(),
+                    R.style.MySpinnerDatePickerStyle,
+                    DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                        updateBirthday(year, month.plus(1), dayOfMonth)
+                    },
+                    viewModel.person.birthday?.year ?: LocalDate.now().year,
+                    viewModel.person.birthday?.monthValue?.minus(1)
+                        ?: LocalDate.now().monthValue.minus(1),
+                    viewModel.person.birthday?.dayOfMonth ?: LocalDate.now().dayOfMonth
+                ).show()
+            }
+
+        })
+
+        viewModel.navigateToFamilyFragment.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                findNavController().navigate(AddPersonFragmentDirections.actionAddPersonFragmentToFamilyFragment())
+            }
+        })
+    }
+
+    private fun updateBirthday(year: Int, month: Int, dayOfMonth: Int) {
+        viewModel.setBirthday(year, month, dayOfMonth)
+        editBirthday.setText(viewModel.person.birthday.toString())
     }
 
 }
